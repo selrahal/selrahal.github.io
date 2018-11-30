a175c26dc1a89d9688d5c9aca8dca10699274ff4
Salem Elrahal <salem.elrahal@gmail.com>
2018-11-30 14:37:24 -0500

    added code

--- js/app.js
+++ js/app.js
@@ -3,3 +3,53 @@ for (var i = 0; i < posts.length; i++) {
  posts[i].style.animationName = "slidein"
  posts[i].style.animationDelay = (i*5) + "0ms"
 }
+
+
+function loadCss(filename) {
+    var fileref = document.createElement("link")
+    fileref.setAttribute("rel", "stylesheet")
+    fileref.setAttribute("type", "text/css")
+    fileref.setAttribute("href", filename)
+    document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+
+
+function loadNES() {
+    loadCss("https://unpkg.com/nes.css@0.0.2/css/nes.min.css");
+}
+
+
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+var konamiCodePosition = 0;
+
+document.addEventListener('keydown', function(e) {
+  var key = allowedKeys[e.keyCode];
+  var requiredKey = konamiCode[konamiCodePosition];
+  if (key == requiredKey) {
+    konamiCodePosition++;
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+
+function activateCheats() {
+	loadNES();
+}
+
